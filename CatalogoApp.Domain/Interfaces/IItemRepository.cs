351d70c4feea19beea407d1bacf1012827cade43
enriq <enriq@DESKTOP45>
2026-05-19 08:37:21 -0600

    CREATE: Domain ItemRepository

---
+++ CatalogoApp.Domain/Interfaces/IItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using CatalogoApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatalogoApp.Domain.Interfaces
+{
+    public interface IItemRepository
+    {
+        List<Item> ObtenerTodos();
+        Item? ObtenerPorId(int id);
+        void Agregar(Item item);
+        void Eliminar(int id);
+    }
+}
